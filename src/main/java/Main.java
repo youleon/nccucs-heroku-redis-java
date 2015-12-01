12c879421531fd54714e2fe85c289374b866e84a
NoelBao <noeleon930@gmail.com>
2015-12-01 23:01:44 +0800

    fix!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,7 +16,7 @@ public class Main {
         URI redisURI = new URI(System.getenv("REDIS_URL"));
         Jedis client = new Jedis(redisURI);
 
-        get("/", (req, res) -> "Hello NCCUCS!");
+        get("/", (req, res) -> "<h1>To use simple SET/GET/DEL on heroku-redis, try...</h1> <h2>/set/KEY/VALUE</h2> <h2>/get/KEY</h2> <h2>/getall</h2> <h2>/del/KEY</h2>");
 
         get("/set/:key/:value", (req, res) -> client.set(req.params(":key"), req.params(":value")));
 
@@ -28,7 +28,7 @@ public class Main {
 
         get("/del/:key", (req, res) -> client.del(req.params(":key")));
 
-        get("/expire/:key/:sec", (req, res) -> client.expire(req.params(":key"), Integer.parseInt(req.params(":value"))));
+        get("/expire/:key/:sec", (req, res) -> client.expire(req.params(":key"), Integer.parseInt(req.params(":sec"))));
 
         get("/ttl/:key", (req, res) -> client.ttl(req.params(":key")));
 
